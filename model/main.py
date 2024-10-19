7537d533d4ef436ce9c34267ed0c00f6a24b337a
drgmitev <drgmitev@gmail.com>
2024-10-19 16:15:20 +0300

    feat: flask server

--- model/main.py
+++ model/main.py
@@ -20,3 +20,19 @@ env=BlackJackPlayEnv()
 model = PPO.load("Cards_PPO", env)
 
 obs = env.reset()
+
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/reset_game', methods=['POST'])
+def reset_game():
+    env.reset()
+    return jsonify({"result": True}), 200
+
+@app.route('/start_game', methods=['POST'])
+def start_game(): 
+    data = request.get_json()
+    player_cards = data.get('player_cards'), dealer_card = data.get('dealer_card')
+    
